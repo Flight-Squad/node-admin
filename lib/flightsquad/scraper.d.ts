c07f73b44076ebc51fa83e75ddc5fd555818293a
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-16 20:19:49 +0000

    Export agents/amazon/batch

--- lib/flightsquad/scraper.d.ts
+++ lib/flightsquad/scraper.d.ts
@@ -1,9 +1,6 @@
 import { TripGroupQuery } from '@flight-squad/common';
-import { BatchQueue } from '../agents/amazon/batch';
 export interface TripScraperQuery extends TripGroupQuery {
     provider: string;
     /** id of the trip group this trip query belongs to */
     group: string;
-}
-export declare class ScraperQueue extends BatchQueue<TripScraperQuery> {
 }
