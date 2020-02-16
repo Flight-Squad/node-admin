cd80e19895160044ed99902535dee75ad2fae9f7
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-16 04:44:50 +0000

    Add comment explaining entity.ts purpose

--- src/entity.ts
+++ src/entity.ts
@@ -1,3 +1,10 @@
+/*
+    These are interfaces that I don't know where to put.
+    I think these should be moved to @flight-squad/common
+    because they aren't logically coupled to third-party admin
+    libraries.
+ */
+
 export interface ConfigFunc<EntityT, ConfigT> {
     (config: ConfigT): EntityT;
 }
