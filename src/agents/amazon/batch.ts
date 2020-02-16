6afd14dcd6549e516ea0850d1067866855ab1b5f
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-16 04:29:15 +0000

    Add AWS batch queue

---
+++ src/agents/amazon/batch.ts
@@ -0,0 +1,58 @@
+import AWS from 'aws-sdk';
+import { Queue } from '../../queue';
+
+export interface BatchQueueIdentifiers {
+    /**
+     * The job definition used by this job queue.
+     * This value can be either a `name:revision` or the
+     * Amazon Resource Name (ARN) for the job definition.
+     */
+    jobDefinition: string;
+    /**
+     * The name of the job represented in this job queue.
+     * The first character must be alphanumeric, and up to 128 letters
+     * (uppercase and lowercase), numbers, hyphens, and underscores are allowed.
+     */
+    jobName: string;
+    /**
+     * The job queue into which the job is submitted.
+     * You can specify either the name or the Amazon Resource Name (ARN) of the queue.
+     */
+    jobQueue: string;
+}
+
+export class BatchQueue<T> implements Queue<T> {
+    push(data: T, callback?: (err: AWS.AWSError, data: AWS.Batch.SubmitJobResponse) => void): void {
+        const jobCallback = (err: AWS.AWSError, data: AWS.Batch.SubmitJobResponse): void => {
+            if (callback) callback(err, data);
+        };
+
+        try {
+            this.batch.submitJob(
+                {
+                    jobDefinition: '',
+                    jobName: '',
+                    jobQueue: '',
+                    parameters: {
+                        data: JSON.stringify(data),
+                    },
+                },
+                jobCallback,
+            );
+        } catch (e) {}
+    }
+
+    /**
+     *
+     * @param data
+     * @param callback called for each item in `data`
+     */
+    pushAll(data: T[], callback?: (err: AWS.AWSError, data: AWS.Batch.SubmitJobResponse) => void): void {
+        for (const item of data) {
+            this.push(item, callback);
+        }
+    }
+
+    private batch = new AWS.Batch({ apiVersion: '2016-08-10' });
+    constructor(readonly props: BatchQueueIdentifiers) {}
+}
