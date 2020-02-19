66ef21099c8125820e5f0a5a14509a061ec50e4a
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-19 16:59:31 +0000

    Add ScraperQueryHandler and QueryConfig

--- lib/flightsquad/scraper.d.ts
+++ lib/flightsquad/scraper.d.ts
@@ -1,6 +1,33 @@
 import { TripGroupQuery } from '@flight-squad/common';
+import { Queue } from '../queue';
+import { Firebase } from '../agents';
 export interface TripScraperQuery extends TripGroupQuery {
     provider: string;
     /** id of the trip group this trip query belongs to */
     group: string;
+}
+export declare enum ScraperDataTargets {
+    Customer = "customer",
+    DatabaseOnly = "database_only"
+}
+export interface QueryConfig {
+    chatsquad: string;
+    pricesquad: string;
+    businessStrategy: {
+        doc: string;
+        sheetNames: {
+            discount: string;
+        };
+    };
+    dataTarget: string | ScraperDataTargets;
+}
+export interface ConfiguredScraperQuery extends TripScraperQuery {
+    config: QueryConfig;
+}
+export declare class ScraperQueryHandler implements Queue<TripScraperQuery> {
+    readonly Db: Firebase;
+    readonly config: QueryConfig;
+    push(data: TripScraperQuery): Promise<void>;
+    pushAll(data: TripScraperQuery[]): Promise<void>;
+    constructor(Db: Firebase, config: QueryConfig);
 }
--- src/flightsquad/scraper.ts
+++ src/flightsquad/scraper.ts
@@ -1,7 +1,50 @@
 import { TripGroupQuery } from '@flight-squad/common';
+import { Queue } from '../queue';
+import { Firebase } from '../agents';
 
 export interface TripScraperQuery extends TripGroupQuery {
     provider: string;
     /** id of the trip group this trip query belongs to */
     group: string;
+}
+
+export enum ScraperDataTargets {
+    Customer = 'customer',
+    DatabaseOnly = 'database_only',
+}
+
+export interface QueryConfig {
+    chatsquad: string;
+    pricesquad: string;
+    businessStrategy: {
+        doc: string;
+        sheetNames: {
+            discount: string;
+        };
+    };
+    dataTarget: string | ScraperDataTargets;
+}
+
+export interface ConfiguredScraperQuery extends TripScraperQuery {
+    config: QueryConfig;
+}
+
+export class ScraperQueryHandler implements Queue<TripScraperQuery> {
+    async push(data: TripScraperQuery): Promise<void> {
+        await this.Db.firestore
+            .collection('scraper_queries')
+            .doc()
+            .create({
+                ...data,
+                config: this.config,
+            });
+    }
+
+    async pushAll(data: TripScraperQuery[]): Promise<void> {
+        for (const item of data) {
+            await this.push(item);
+        }
+    }
+
+    constructor(readonly Db: Firebase, readonly config: QueryConfig) {}
 }
